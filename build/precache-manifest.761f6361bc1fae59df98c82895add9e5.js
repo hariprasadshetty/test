7da8d576834b6a898b5581000239b9492e916a26
hariprasadshetty <hprasad2191@gmail.com>
2020-03-20 21:41:34 +0530

    cr

---
+++ build/precache-manifest.761f6361bc1fae59df98c82895add9e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cdd387d9037181c9eb834e7cbdc649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d30528efa478dd40547",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66c4d723d19fe3ae70b8",
+    "url": "/static/js/2.2377b599.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2377b599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d30528efa478dd40547",
+    "url": "/static/js/main.b0e52097.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
